85567f37f7ef1957d68c9fbe8b3179f881662b21
DinhDinh <dinwork04@gmail.com>
2025-11-02 23:59:12 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const contractRoutes = require('./app/routes/contract.route');
+const contractRoutes = require('./app/routes/contact.route');
 const ApiError = require('./app/api-error');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/contracts', contractRoutes);
+app.use('/api/contacts', contractRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: 
